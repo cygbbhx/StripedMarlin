85c72a118ab1329162c83053ff93db0efc709ff3
jimiracle <jimiracle@yonsei.ac.kr>
2024-07-22 04:59:22 +0000

    Add ASSIST

--- src/models/models.py
+++ src/models/models.py
@@ -18,5 +18,18 @@ def get_model(model_name: str, config: Dict, device: str):
         return HuBERT.get_model()
     elif model_name == "WavLM":
         return WavLM_MFA.WavLM_MFA(freeze=False)
+    elif model_name == "AASIST":
+        model_config = {
+        #"model_path": "/home/work/StripedMarlin/jimin/StripedMarlin/src/models/weights/AASIST-L.pth",
+        "freeze": False, 
+        "architecture": "AASIST",
+        "nb_samp": 64600,
+        "first_conv": 128,
+        "filts": [70, [1, 32], [32, 32], [32, 24], [24, 24]],
+        "gat_dims": [24, 32],
+        "pool_ratios": [0.4, 0.5, 0.7, 0.5],
+        "temperatures": [2.0, 2.0, 100.0, 100.0]
+        }
+        return AASIST.get_model(model_config, device)
     else:
         raise ValueError(f"Model '{model_name}' not supported")
