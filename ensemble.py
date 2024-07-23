1d04a977f6991586f4b5c5d352fb3f4e5c0b6979
cygbbhx <cygbbhx@yonsei.ac.kr>
2024-07-23 22:11:31 +0000

    Add Conda Env files

--- ensemble.py
+++ ensemble.py
@@ -1,27 +1,37 @@
 import pandas as pd
 from datetime import datetime, timedelta, timezone
-import os 
+import os
+import argparse
 
+# Define the timezone
 KST = timezone(timedelta(hours=9))
 current_time_kst = datetime.now(KST)
 submission_time = current_time_kst.strftime('%m%d-%H%M')
 
-# Load the two CSV files
-file1 = 'ensemble/ensemble_0718-1713.csv'
-file2 = 'ensemble/ensemble_0719-0948.csv'
+# Set up argument parser
+parser = argparse.ArgumentParser(description='Ensemble two CSV files.')
+parser.add_argument('file1', type=str, help='Path to the first CSV file')
+parser.add_argument('file2', type=str, help='Path to the second CSV file')
+args = parser.parse_args()
 
-df1 = pd.read_csv(file1)
-df2 = pd.read_csv(file2)
+# Load the CSV files
+df1 = pd.read_csv(args.file1)
+df2 = pd.read_csv(args.file2)
 
+# Sort dataframes by 'id' column
 df1.sort_values(by='id', inplace=True)
 df2.sort_values(by='id', inplace=True)
 
+# Ensure the 'id' columns match
 assert (df1['id'] == df2['id']).all(), "The IDs in the two files do not match."
 
+# Create the ensemble dataframe
 df_ensemble = df1.copy()
 df_ensemble['fake'] = (df1['fake'] + df2['fake']) / 2
 df_ensemble['real'] = (df1['real'] + df2['real']) / 2
 
-df_ensemble.to_csv(f'ensemble/ensemble_{submission_time}.csv', index=False)
+# Save the ensembled dataframe to a new CSV file
+output_path = f'outputs/ensemble_{submission_time}.csv'
+df_ensemble.to_csv(output_path, index=False)
 
-print(f'Ensembled results saved')
+print(f'Ensembled results saved to {output_path}')
